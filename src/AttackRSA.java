13e88cb76f0d9b2441a211d902c1b355eec6dcce
Clement Mercier <clemmercier@hotmail.com>
2015-12-22 00:03:22 +0100

    improvement CRT

--- src/AttackRSA.java
+++ src/AttackRSA.java
@@ -46,9 +46,46 @@ public class AttackRSA {
 	 * @return The same message that was sent to each receiver.
 	 */
 	private static BigInteger recoverMessage(BigInteger[] N, BigInteger[] e,
-			BigInteger[] c) {			
+			BigInteger[] c) {
 		
-		BigInteger C = BigInteger.ZERO;
+		/*
+		 * the current setup finds a number x such that:
+		 *	x = 2 mod 5, x = 3 mod 7, x = 4 mod 9, and x = 5 mod 11
+		 * note that the values in mods must be mutually prime
+		 */	    
+	    
+	    //M is the product of the mods
+	    BigInteger M = BigInteger.ONE;
+	    for(int i = 0; i < N.length; i++)
+	      M = M.multiply(N[i]);
+	    
+	    BigInteger[] multInv = new BigInteger[c.length];
+	    
+	    /*
+	     * this loop applies the Euclidean algorithm to each pair of M/mods[i] and mods[i]
+	     * since it is assumed that the various mods[i] are pairwise coprime,
+	     * the end result of applying the Euclidean algorithm will be
+	     * gcd(M/mods[i], mods[i]) = 1 = a(M/mods[i]) + b(mods[i]), or that a(M/mods[i]) is
+	     * equivalent to 1 mod (mods[i]). This a is then the multiplicative
+	     * inverse of (M/mods[i]) mod mods[i], which is what we are looking to multiply
+	     * by our constraint constraints[i] as per the Chinese Remainder Theorem
+	     * euclidean(M/mods[i], mods[i])[0] returns the coefficient a
+	     * in the equation a(M/mods[i]) + b(mods[i]) = 1
+	     */
+	    for(int i = 0; i < multInv.length; i++)
+	      multInv[i] = euclidean(M.divide(N[i]), N[i])[0];
+	    
+	    BigInteger x = BigInteger.ZERO;
+	    
+	    //x = the sum over all given i of (M/mods[i])(constraints[i])(multInv[i])
+	    for(int i = 0; i < N.length; i++)
+	      x = x.add((M.divide(N[i])).multiply(c[i].multiply(multInv[i])));
+	    
+	    x = leastPosEquiv(x, M);
+	    
+	    //System.out.println("x is equivalent to " + x + " mod " + M);
+		
+		/*BigInteger C = new BigInteger("993754199");
 		
 		System.out.println("Debut");
 		System.out.println(C.toString());
@@ -57,11 +94,73 @@ public class AttackRSA {
 			      ){
 			C = C.add(new BigInteger("1"));
 			System.out.println(C.toString());
-		}
+		}*/
 		
-		return CubeRoot.cbrt(C);
+		return CubeRoot.cbrt(x);
 		
 		//return BigInteger.ZERO;
 	}
+	
+	  /*
+	   * performs the Euclidean algorithm on a and b to find a pair of coefficients
+	   * (stored in the output array) that correspond to x and y in the equation
+	   * ax + by = gcd(a,b)
+	   * constraint: a > b
+	   */
+	  public static BigInteger[] euclidean(BigInteger a, BigInteger b)
+	  {
+	    if(b.compareTo(a) > 0)
+	    {
+	      //reverse the order of inputs, run through this method, then reverse outputs
+	      BigInteger[] coeffs = euclidean(b, a);
+	      BigInteger[] output = {coeffs[1], coeffs[0]};
+	      return output;
+	    }
+
+	    BigInteger q = a.divide(b);
+	    //a = q*b + r --> r = a - q*b
+	    BigInteger r = a.subtract(q.multiply(b));
+	    
+	    //when there is no remainder, we have reached the gcd and are done
+	    if(r == BigInteger.ZERO)
+	    {
+	      BigInteger[] output = {BigInteger.ZERO, BigInteger.ONE};
+	      return output;
+	    }
+	    
+	    //call the next iteration down (b = qr + r_2)
+	    BigInteger[] next = euclidean(b, r);
+	    
+	    BigInteger[] output = {next[1], next[0].subtract(q.multiply(next[1]))};
+	    return output;
+	  }
+	  
+	  //finds the least positive integer equivalent to a mod m
+	  public static BigInteger leastPosEquiv(BigInteger a, BigInteger m)
+	  {
+	    //a eqivalent to b mod -m <==> a equivalent to b mod m
+	    if(m.compareTo(BigInteger.ZERO) < 0)
+	      return leastPosEquiv(a, new BigInteger("-1").multiply(m));
+	    //if 0 <= a < m, then a is the least positive integer equivalent to a mod m
+	    if(a.compareTo(BigInteger.ZERO) >= 0 && a.compareTo(m) < 0)
+	      return a;
+	    
+	    //for negative a, find the least negative integer equivalent to a mod m
+	    //then add m
+	    if(a.compareTo(BigInteger.ZERO) < 0)
+	      return new BigInteger("-1").multiply(leastPosEquiv(new BigInteger("-1").multiply(a), m)).add(m);
+	    
+	    //the only case left is that of a,m > 0 and a >= m
+	    
+	    //take the remainder according to the Division algorithm
+	    BigInteger q = a.divide(m);
+	    
+	    /*
+	     * a = qm + r, with 0 <= r < m
+	     * r = a - qm is equivalent to a mod m
+	     * and is the least such non-negative number (since r < m)
+	     */
+	    return a.subtract(q.multiply(m));
+	  }
 
 }
